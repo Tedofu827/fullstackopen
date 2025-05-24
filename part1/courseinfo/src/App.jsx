4c793eb61a6ef3ca1f06bda5a1697005acf425bd
Tedofu827 <khangnguyentran250703@gmail.com>
2025-05-24 22:43:55 +0700

    start-part2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,37 @@
-const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
-
-  return <Course course={course} />;
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
 };
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <p>
+        {parts[0].name}: {parts[0].exercises}
+      </p>
+      <p>
+        {parts[1].name}: {parts[1].exercises}
+      </p>
+      <p>
+        {parts[2].name}: {parts[2].exercises}
+      </p>
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises{": "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
+};
+return (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
 
 export default App;
